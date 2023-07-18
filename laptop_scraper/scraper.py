11f56fdf1178408c50a0173b749ee5ae407cead7
André Vicente <isquicha@gmail.com>
2023-07-18 00:48:07 -0300

    refact: separa o scrap em funções

--- laptop_scraper/scraper.py
+++ laptop_scraper/scraper.py
@@ -4,35 +4,47 @@ from .constants import BRANDS
 from .entities import Product
 
 
-def get_product_from_div(div: BeautifulSoup) -> Product:
-    name = div.find("a", {"class": "title"}).text
-    brand = ""
+def get_product_name(div: BeautifulSoup) -> str:
+    return div.find("a", {"class": "title"}).text or ""
+
+
+def get_product_brand(product_name: str) -> str:
     for brand_slug, brand_name in BRANDS.items():
-        if brand_slug in name.lower():
-            brand = brand_name
-            break
-    price = float(
+        if brand_slug in product_name.lower():
+            return brand_name
+    return ""
+
+
+def get_product_price(div: BeautifulSoup) -> float:
+    return float(
         div.find("h4", {"class": "pull-right price"}).text.replace("$", "")
     )
-    description = div.find("p", {"class": "description"}).text
-    rating = div.find("div", {"class": "ratings"}).find_all("p")[-1][
-        "data-rating"
-    ]
-    reviews = div.find("p", {"class": "pull-right"}).text
-    image = div.find("img", {"class": "img-responsive"})["src"]
-    image = f"https://webscraper.io{image}"
-    url = div.find("a", {"class": "title"})["href"]
-    url = f"https://webscraper.io{url}"
-    return {
-        "name": name,
-        "brand": brand,
-        "price": price,
-        "description": description,
-        "rating": rating,
-        "reviews": reviews,
-        "image": image,
-        "url": url,
-    }
+
+
+def get_product_description(div: BeautifulSoup) -> str:
+    return div.find("p", {"class": "description"}).text or ""
+
+
+def get_product_rating(div: BeautifulSoup) -> float:
+    ratings_div = div.find("div", {"class": "ratings"})
+    ratings_paragraphs = ratings_div.find_all("p")
+    rating_holder = ratings_paragraphs[-1]
+    rating = rating_holder["data-rating"]
+    return rating or 0.0
+
+
+def get_product_reviews(div: BeautifulSoup) -> str:
+    return div.find("p", {"class": "pull-right"}).text or ""
+
+
+def get_product_image(div: BeautifulSoup) -> str:
+    relative_url = div.find("img", {"class": "img-responsive"})["src"] or ""
+    return f"https://webscraper.io{relative_url}"
+
+
+def get_product_url(div: BeautifulSoup) -> str:
+    relative_url = div.find("a", {"class": "title"})["href"] or ""
+    return f"https://webscraper.io{relative_url}"
 
 
 def scrape_page(page_content: str) -> list[Product]:
@@ -41,7 +53,16 @@ def scrape_page(page_content: str) -> list[Product]:
     products_div = header.parent.div
 
     return [
-        get_product_from_div(div)
+        {
+            "name": get_product_name(div),
+            "brand": get_product_brand(get_product_name(div)),
+            "price": get_product_price(div),
+            "description": get_product_description(div),
+            "rating": get_product_rating(div),
+            "reviews": get_product_reviews(div),
+            "image": get_product_image(div),
+            "url": get_product_url(div),
+        }
         for div in products_div.find_all(
             "div", {"class": "col-sm-4 col-lg-4 col-md-4"}
         )
